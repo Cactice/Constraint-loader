0e1ac37e207aa995cae73694dcc27ad381affeb0
cactice <14835424+Cactice@users.noreply.github.com>
2023-12-29 13:44:29 +0900

    Update code.ts

--- code.ts
+++ code.ts
@@ -1,61 +1,75 @@
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+const xMapper: Record<
+  ConstraintType,
+  ({ bbox, parentBbox }: { bbox: Rect; parentBbox: Rect }) => unknown
+> = {
+  MIN: ({ bbox, parentBbox }) => ({ Left: bbox.x - parentBbox.x }),
+  CENTER: ({ bbox, parentBbox }) => ({ Center: bbox.x - parentBbox.x }),
+  MAX: ({ bbox, parentBbox }) => ({
+    Right: bbox.x + bbox.width - (parentBbox.x + parentBbox.width),
+  }),
+  STRETCH: ({ bbox, parentBbox }) => ({
+    LeftAndRight: {
+      left: bbox.x - parentBbox.x,
+      right: bbox.x + bbox.width - (parentBbox.x + parentBbox.width),
+    },
+  }),
+  SCALE: () => "Scale",
+} as const;
+const yMapper: Record<
+  ConstraintType,
+  ({ bbox, parentBbox }: { bbox: Rect; parentBbox: Rect }) => unknown
+> = {
+  MIN: ({ bbox, parentBbox }) => ({ Top: bbox.y - parentBbox.y }),
+  CENTER: ({ bbox, parentBbox }) => ({ Center: bbox.y - parentBbox.y }),
+  MAX: ({ bbox, parentBbox }) => ({
+    Bottom: bbox.y + bbox.height - (parentBbox.y + parentBbox.height),
+  }),
+  STRETCH: ({ bbox, parentBbox }) => ({
+    LeftAndRight: {
+      left: bbox.y - parentBbox.y,
+      right: bbox.y + bbox.height - (parentBbox.y + parentBbox.height),
+    },
+  }),
+  SCALE: () => "Scale",
+} as const;
 
-// Runs this code if the plugin is run in Figma
+const attributeGetter = (component: SceneNode) => {
+  const constraints = "constraints" in component && component.constraints;
+  const bbox =
+    "absoluteBoundingBox" in component && component.absoluteBoundingBox;
+  const parentBbox =
+    component.parent &&
+    "absoluteBoundingBox" in component.parent &&
+    component.parent.absoluteBoundingBox;
+
+  if (constraints && bbox && parentBbox) {
+    const json = {
+      x: xMapper[constraints.horizontal]({ bbox, parentBbox }),
+      y: yMapper[constraints.vertical]({ bbox, parentBbox }),
+    };
+    return { [component.name]: json };
+  } else {
+    return {};
+  }
+};
+
+const recurseChildren = (
+  parent: SceneNode,
+  func: (child: SceneNode) => Record<string, { x: unknown; y: unknown }>
+): Record<string, { x: unknown; y: unknown }> => {
+  if ("children" in parent) {
+    return parent.children.reduce((acc, child) => {
+      return { ...acc, ...func(child), ...recurseChildren(child, func) };
+    }, {});
+  }
+  return {};
+};
+
 if (figma.editorType === "figma") {
-  // This plugin creates 5 rectangles on the screen.
-  const numberOfRectangles = 5;
-
-  const nodes: SceneNode[] = [];
-  for (let i = 0; i < numberOfRectangles; i++) {
-    const rect = figma.createRectangle();
-    rect.x = i * 150;
-    rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-    figma.currentPage.appendChild(rect);
-    nodes.push(rect);
-  }
-  figma.currentPage.selection = nodes;
-  figma.viewport.scrollAndZoomIntoView(nodes);
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-}
-// Runs this code if the plugin is run in FigJam
-if (figma.editorType === "figjam") {
-  // This plugin creates 5 shapes and 5 connectors on the screen.
-  const numberOfShapes = 5;
-
-  const nodes: SceneNode[] = [];
-  for (let i = 0; i < numberOfShapes; i++) {
-    const shape = figma.createShapeWithText();
-    // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-    shape.shapeType = "ROUNDED_RECTANGLE";
-    shape.x = i * (shape.width + 200);
-    shape.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-    figma.currentPage.appendChild(shape);
-    nodes.push(shape);
-  }
-
-  for (let i = 0; i < numberOfShapes - 1; i++) {
-    const connector = figma.createConnector();
-    connector.strokeWeight = 8;
-
-    connector.connectorStart = {
-      endpointNodeId: nodes[i].id,
-      magnet: "AUTO",
-    };
-
-    connector.connectorEnd = {
-      endpointNodeId: nodes[i + 1].id,
-      magnet: "AUTO",
-    };
-  }
-
-  figma.currentPage.selection = nodes;
-  figma.viewport.scrollAndZoomIntoView(nodes);
+  const json = figma.currentPage.selection.map((child) =>
+    recurseChildren(child, attributeGetter)
+  )[0];
+  console.log(JSON.stringify(json));
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
